ef1a9d6d4a4a1831a595dc7835f085ef5d958723
mmesnjak <misel.mesnjak@asseco-see.hr>
2018-10-31 13:59:13 +0100

    Initial commit

---
+++ personal-inventory-backend/src/main/java/com/miselmesnjak/personalinventorybackend/PersonalInventoryBackendApplication.java
@@ -0,0 +1,12 @@
+package com.miselmesnjak.personalinventorybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalInventoryBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PersonalInventoryBackendApplication.class, args);
+  }
+}
---
+++ personal-inventory-backend/src/test/java/com/miselmesnjak/personalinventorybackend/PersonalInventoryBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.miselmesnjak.personalinventorybackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalInventoryBackendApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
